e8b746b18f23c1e46421e437dfbf0edcb228fb8c
anrajin <an.rajin@gmail.com>
2023-04-17 23:31:24 +0600

    dashboard and signup component added

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    SignupComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
